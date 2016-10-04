b10de8dc78c0ebca1c7dc5ba36fba8942c280445
ChristerNilsson <janchrister.nilsson@gmail.com>
2016-10-04 15:51:12 +0200

    x

--- Bitmap_Assert/L3_5.py
+++ Bitmap_Assert/L3_5.py
@@ -7,61 +7,85 @@ def lektion3_5(ass):
     with ass.check("for01"): 
         if ass.errors(): return
         for i in range(10):
-            rect(5+20*i,5, 10,10)
+            x = 5+20*i
+            rect(x,5, 10,10)
             
     with ass.check("for02"): 
         if ass.errors(): return
         for i in range(10):
-            rect(5,5+20*i, 10,10)
+            y = 5+20*i
+            rect(5,y, 10,10)
             
     with ass.check("for03"): 
         if ass.errors(): return
         for i in range(10):
-            rect(5+20*i,5+20*i, 10,10)
+            x = 5+20*i
+            y = 5+20*i
+            rect(x,y, 10,10)
             
     with ass.check("for04"): 
         if ass.errors(): return
         for i in range(10):
             for j in range(10):
-                rect(5+20*i,5+20*j, 10,10)
+                x = 5+20*i
+                y = 5+20*j
+                rect(x,y, 10,10)
                           
     with ass.check("for05"): 
         if ass.errors(): return
         rectMode(CENTER)
         for i in range(10):
-            rect(10+20*i,10, 2*i,2*i)
+            x = 10+20*i
+            y = 10
+            w = 2*i
+            h = 2*i
+            rect(x,y, w,h)
 
     with ass.check("for06"): 
         if ass.errors(): return
         rectMode(CENTER)
         for i in range(10):
             fc(i/10.0,0,0)
-            rect(10+20*i,10, 2*i,2*i)
+            x = 10+20*i
+            y = 10
+            w = 2*i
+            h = 2*i
+            rect(x,y,w,h)
 
     with ass.check("for07"): 
         if ass.errors(): return
         for i in range(10):
             fc(i/10.0,0,0)
-            circle(10+20*i,10, i)
+            x = 10+20*i
+            y = 10
+            r = i
+            circle(x,y,r)
             
     with ass.check("for08"): 
         if ass.errors(): return
         for i in range(10,0,-1):
             fc(i/10.0,0,0)
-            circle(100,100, 10*i)
+            r = 10 * i
+            circle(100,100, r)
             
     with ass.check("for09"): 
         if ass.errors(): return
         for i in range(10,0,-1):
             fc(i/10.0,0,0)
-            circle(10*i,10*i, 10*i)
+            x = 10*i
+            y = 10*i
+            r = 10*i
+            circle(x,y,r)
             
     with ass.check("for10"): 
         if ass.errors(): return
         for i in range(10):
             for j in range(10):
                 fc(i/10.0,j/10.0,0)
-                circle(10+20*i,10+20*j, (i+j)/2)
+                x = 10+20*i
+                y = 10+20*j
+                r = (i+j)/2
+                circle(x,y,r)
                 
     with ass.check("for11"): 
         if ass.errors(): return
@@ -72,7 +96,10 @@ def lektion3_5(ass):
                 with push():
                     translate(10+20*i,10+20*j)
                     rd(5*(i+j))
-                    fc(i/9.0,j/9.0,0)
+                    r = i/9.0
+                    g = j/9.0
+                    b = 0 
+                    fc(r,g,b)
                     rect(0,0, 10,10)
                 
     with ass.check("for12"): 
@@ -81,8 +108,11 @@ def lektion3_5(ass):
         sc(1)
         translate(100,100)
         for i in range(18,-1,-1):
-            fc(1.0*i/18,0,0)
-            rect(0,0,70+5*i,70+5*i)
+            r = 1.0*i/18
+            fc(r,0,0)
+            w = 70+5*i
+            h = 70+5*i
+            rect(0,0, w,h)
             rd(5)
         
                                                 
