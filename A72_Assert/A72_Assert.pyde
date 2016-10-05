5088efd0cd4cde04b163cfad96afd2e9117b441b
ChristerNilsson <janchrister.nilsson@gmail.com>
2016-10-05 11:32:06 +0200

    Linjära samband

--- A72_Assert/A72_Assert.pyde
+++ A72_Assert/A72_Assert.pyde
@@ -1,7 +1,8 @@
 from L1 import L1
 from L2 import L2
 from L3 import L3
+from L4 import L4
 
-L3()
+L4()
 
 print "Klart!"
---
+++ A72_Assert/L4.py
@@ -0,0 +1,50 @@
+# linjära samband
+
+###################
+
+def L4():
+
+    def d(x): return 1
+    assert d(0) == 1 
+    assert d(1) == 1 
+    assert d(2) == 1 
+
+    def e(x): return 100
+    assert e(0) == 100 
+    assert e(1) == 100 
+    assert e(2) == 100 
+
+    def f(x): return x
+    assert f(0) == 0 
+    assert f(1) == 1 
+    assert f(2) == 2 
+    
+    def g(x): return x+1
+    assert g(0) == 1
+    assert g(1) == 2
+    
+    def h(x): return 2*x
+    assert h(0) == 0
+    assert h(1) == 2
+    assert h(2) == 4
+    
+    def i(x): return 2*x+1
+    assert i(0) == 1
+    assert i(1) == 3
+    assert i(2) == 5
+    
+    def j(x): return 10*x
+    assert j(0) == 0
+    assert j(1) == 10
+    assert j(2) == 20
+    
+    def k(x): return 20*x+10
+    assert k(0) == 10
+    assert k(1) == 30
+    assert k(2) == 50
+    
+    def l(x): return 11*x+11
+    assert l(0) == 11
+    assert l(1) == 22
+    assert l(2) == 33
+    
